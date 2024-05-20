4aaf5031347d20a1d8133f4713877187f2e0ddde
Yatochka <philipchef13@gmail.com>
2024-05-21 00:45:44 +0300

    add geolocation testing

--- src/app/d/route.ts
+++ src/app/d/route.ts
@@ -4,7 +4,10 @@ async function GET(req: Request) {
     const location = geolocation(req)
 
     console.log(JSON.stringify(req.headers.get('x-forwarded-for')))
-    return new Response(JSON.stringify(req.headers.get('x-forwarded-for')))
+    return new Response(
+        JSON.stringify({ ...location, ip: req.headers.get('x-forwarded-for') }),
+        {},
+    )
 }
 
 export { GET }
