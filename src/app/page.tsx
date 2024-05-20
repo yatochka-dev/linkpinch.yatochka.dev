8fc9be985351b52b13db82c3489ccf48d6d61afb
Yatochka <philipchef13@gmail.com>
2024-05-20 14:02:38 +0300

    feat(landing)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { Box, Typography } from '@mui/material'
+
 export default function Home() {
-    return 'hello'
+    return (
+        <Box
+            sx={{
+                minHeight: 'calc(100vh - 200px)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Typography variant={'h1'}>Hello World</Typography>
+        </Box>
+    )
 }
