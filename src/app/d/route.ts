8126e37835bfb4dde8556367349767d091487c2c
Yatochka <philipchef13@gmail.com>
2024-05-21 00:45:07 +0300

    add geolocation testing

--- src/app/d/route.ts
+++ src/app/d/route.ts
@@ -1,4 +1,8 @@
+import { geolocation } from '@vercel/edge'
+
 async function GET(req: Request) {
+    const location = geolocation(req)
+
     console.log(JSON.stringify(req.headers.get('x-forwarded-for')))
     return new Response(JSON.stringify(req.headers.get('x-forwarded-for')))
 }
