e4ba284770f64863b565f1d0596b1424f640fef7
Yatochka <philipchef13@gmail.com>
2024-05-22 01:17:40 +0300

    fix(dashboard) - page title fetching &quote; -> "

--- src/utils/functools/get-page-title.ts
+++ src/utils/functools/get-page-title.ts
@@ -1,32 +1,41 @@
+import * as cheerio from 'cheerio'
+
 export default async function getPageTitle(
     url: string,
 ): Promise<string | undefined> {
-    const u = new URL(url)
+    try {
+        const u = new URL(url)
 
-    const response = await fetch(u, {
-        method: 'GET',
-        cache: 'no-store',
-        headers: {
-            'User-Agent':
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3',
-        },
-    })
+        const response = await fetch(u, {
+            method: 'GET',
+            cache: 'no-store',
+            headers: {
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3',
+            },
+        })
 
-    if (!response.ok) {
+        if (!response.ok) {
+            return undefined
+        }
+
+        const text = await response.text()
+
+        if (!text) {
+            return undefined
+        }
+
+        const $ = cheerio.load(text)
+
+        const titleElement = $('title').text()
+
+        if (!titleElement) {
+            return undefined
+        }
+
+        return titleElement.trim() || undefined
+    } catch (e) {
+        console.error('Error getting page title', e)
         return undefined
     }
-
-    const text = await response.text()
-
-    if (!text) {
-        return undefined
-    }
-
-    const title = text.match(/<title>([^<]+)<\/title>/i)
-
-    if (!title) {
-        return undefined
-    }
-
-    return title[1] ?? title[0] ?? undefined // The first match is the full title, the second match is the title without the tags
 }
